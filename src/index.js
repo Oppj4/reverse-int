a45afb7a8703833591d0384a7ccc186428e34255
OPPJ4 <geed704@gmail.com>
2022-01-27 01:25:40 +0300

    feat: realize reverse method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (initialNum) {
+  num = initialNum;
+
+  if(num > 0){
+      num = String(num);
+  }
+  else{
+      num *= (-1);
+      num = String(num);
+  }
+
+  let i = num.length;
+  let result = '';
+
+  while(i > 0){
+      result = `${result}${num[i-1]}`;
+      i--;
+  }
+  result = Number(result);
+  return result;
 }
